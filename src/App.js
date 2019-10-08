343e70a2592de568da52b9e19ff1e5dff07a9cf1
Wolgrand Pinto Lordao Neto <wolgrand.lordao@energisa.com.br>
2019-10-08 08:39:48 -0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 
 import TechList from './components/TechList';
+import Facebook from './components/Facebook';
 
 function App() {
-  return <TechList />;
+  return <Facebook />;
 }
 
 export default App;
